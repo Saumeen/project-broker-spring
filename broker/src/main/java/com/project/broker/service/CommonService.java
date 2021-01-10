00c9779dbb3eec58a39d772d2d90aace0d9e82b6
saumeen prajapati <psaumeen@gmail.com>
2021-01-10 21:43:26 +0530

    Zuul server project added

--- broker/src/main/java/com/project/broker/service/CommonService.java
+++
@@ -1,8 +0,0 @@
-package com.project.broker.service;
-
-import com.project.broker.dto.IpDto;
-
-public interface CommonService {
-
-	public String setIpAddress(IpDto ipDto);
-}
--- broker/src/main/resources/table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `broker`.`user` (
-  `idUser` INT NOT NULL AUTO_INCREMENT,
-  `userName` VARCHAR(100) NOT NULL,
-  `passWord` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idUser`));
-  
-  
