0bf618618031304cfa2ba9794bf0dcd01f32249b
saumeen prajapati <psaumeen@gmail.com>
2021-01-10 23:46:11 +0530

    ip config changed

--- zuul-apigateway/src/main/java/com/project/broker/proxy/BrokerFignClientProxy.java
+++ zuul-apigateway/src/main/java/com/project/broker/proxy/BrokerFignClientProxy.java
@@ -3,7 +3,7 @@ package com.project.broker.proxy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-//@RibbonClient(name="project-broker-api")
+
 @FeignClient(name = "project-broker-api",url = "https://broker-micro.herokuapp.com")
 public interface BrokerFignClientProxy {
 
