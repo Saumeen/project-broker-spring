a7fa20e10171384c50ed2f3473053a01ffef9735
saumeen prajapati <psaumeen@gmail.com>
2021-01-10 23:16:33 +0530

    fign updated

--- zuul-apigateway/src/main/java/com/project/broker/proxy/BrokerFignClientProxy.java
+++ zuul-apigateway/src/main/java/com/project/broker/proxy/BrokerFignClientProxy.java
@@ -3,8 +3,8 @@ package com.project.broker.proxy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-@FeignClient(name = "project-broker",url = "http://localhost:8000")
+//@RibbonClient(name="project-broker-api")
+@FeignClient(name = "project-broker-api",url = "https://broker-micro.herokuapp.com")
 public interface BrokerFignClientProxy {
 
 	@PostMapping("/api/master/sendIpAddress")
